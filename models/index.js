f32719a942e22f932ec13206944fbcce683c0d76
Brainybrian316 <brainybrian316@gmail.com>
2022-05-21 18:27:23 -0400

    associations

--- models/index.js
+++ models/index.js
@@ -4,3 +4,28 @@ const Post = require('./Post');
 const Comments = require('./Comments');
 
 // create the associations between the tables
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comments, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comments };
