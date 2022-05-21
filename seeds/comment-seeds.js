3733ae4a10d802b18ab55d57044418d482b504fe
Brainybrian316 <brainybrian316@gmail.com>
2022-05-21 13:27:44 -0400

    seed values

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,7 +1,36 @@
 const { Comment } = require('../models');
 
 const commentData = [
-
+    {
+        user_id: 1,
+        post_id: 2,
+        content: 'This is awesome! I love it!'
+    },
+    {
+        user_id: 2,
+        post_id: 2,
+        content: 'Thank you!'
+    },
+    {
+        user_id: 3,
+        post_id: 1,
+        content: 'I agree!'
+    },
+    {
+        user_id: 1,
+        post_id: 1,
+        content: 'Do you really like it?'
+    },
+    {
+        user_id: 1,
+        post_id: 3,
+        content: 'I use these tips a lot.'
+    },
+    {
+        user_id: 2,
+        post_id: 3,
+        content: 'I have a few tips for a successful tech career too.'
+    }
 ]
 
 const seedComments = () => Comment.bulkCreate(commentData);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,7 +1,21 @@
 const { User } = require('../models/user');
 
 const userData = [
-
+    {
+        username: 'john_doe',
+        email: 'johndoe@tmail.com',
+        password: 'password1'
+    },
+    {
+        username: 'sally_smith',
+        email: 'sallysmith@tmail.com',
+        password: 'password2'
+    },
+    {
+        username: 'spongebob',
+        email: 'spongebob@tmail.com',
+        password: 'password3'
+    }
 ]
 
 const seedUsers = () => User.bulkCreate(userData);
