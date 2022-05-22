d618ac873d0b81272234f6a50f4ea68efb91c3e6
Brainybrian316 <brainybrian316@gmail.com>
2022-05-22 13:20:03 -0400

    signup and login

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('input[name="#email-login"]').value.trim();
+    const password = document.querySelector('input[name="#password-login"]').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
