d271020dcfc9ffb0c1cf1db5ed82d0192de56548
Brainybrian316 <brainybrian316@gmail.com>
2022-05-25 22:25:06 -0400

    comments

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+//  this will check if the user is logged in before running the "next" arrow function of the route
 const withAuth = (req, res, next) => {
     if (!req.session.user_id) {
         res.redirect('/login');
